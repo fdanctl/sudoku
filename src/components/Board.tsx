d546ecf4d7f43ef300e54b88452e0354e8b3d5a8
fdanctl <daniel.a.d.freire.00@gmail.com>
2025-04-07 23:26:06 +0100

    styling make borders thinner

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -44,20 +44,19 @@ export function Board({
               ? "bg-disabled" // disabled color
               : "bg-default"; // default
 
-    const borderLeftWidth = col % 3 === 0 ? "border-l-[3px]" : "border-l-[1px]";
     const borderRightWidth =
-      (col + 1) % 3 === 0 ? "border-r-[3px]" : "border-r-[1px]";
-    const borderTopWidth = row % 3 === 0 ? "border-t-[3px]" : "border-t-[1px]";
+      col !== 8 && (col + 1) % 3 === 0 ? "border-r-[4px]" : "border-r-[1px]";
     const borderBottomWidth =
-      (row + 1) % 3 === 0 ? "border-b-[3px]" : "border-b-[1px]";
+      row !== 8 && (row + 1) % 3 === 0 ? "border-b-[4px]" : "border-b-[1px]";
+
     const display = e === emptyCell ? "grid " : "";
 
-    return `${display}${textColor}${backgroundColor} ${borderLeftWidth} ${borderRightWidth} ${borderTopWidth} ${borderBottomWidth}`;
+    return `${display}${textColor}${backgroundColor} ${borderRightWidth} ${borderBottomWidth}`;
   };
 
   return (
     <div
-      className={`w-[32rem] h-[32rem] grid grid-cols-9 border-3 border-outerBorderColor rounded-sm gap-0 select-none ${style}`}
+      className={`w-[32rem] h-[32rem] grid grid-cols-9 border-6 border-outerBorderColor rounded-sm gap-0 select-none ${style}`}
     >
       {currentGame.map((row, iRow) =>
         row.map((e, iCol) => (
@@ -65,38 +64,6 @@ export function Board({
             onClick={() => onClick(iRow, iCol)}
             key={`${iRow}${iCol}`}
             className={`flex items-center justify-center text-3xl font-bold border-innerBorderColor grid-cols-3 grid-rows-3 ${styleTail(e, iRow, iCol)}`}
-          //          style={{
-          //            color:
-          //              e !== emptyCell && conflicts[iRow][iCol].includes(e)
-          //                ? "red"
-          //                : "",
-          //            background:
-          //              iRow === currentCoords.row && iCol === currentCoords.col
-          //                ? "#265569" // selected
-          //                : e === currentNum &&
-          //                  e !== emptyCell &&
-          //                  settings.togles.highlightIdenticalNumbers
-          //                  ? "#2A6F8C" // same number as selected house
-          //                  : (settings.togles.highlightRow &&
-          //                    iRow === currentCoords.row) ||
-          //                    (settings.togles.highlightCol &&
-          //                      iCol === currentCoords.col) ||
-          //                    (settings.togles.highlightBox &&
-          //                      Math.floor(iRow / 3) ===
-          //                      Math.floor(currentCoords.row / 3) &&
-          //                      Math.floor(iCol / 3) ===
-          //                      Math.floor(currentCoords.col / 3))
-          //                    ? "#637F8C" // same row, same col or same block
-          //                    : e === baseBoard[iRow][iCol] && e !== emptyCell
-          //                      ? "#5C6B73" // disabled color
-          //                      : "#9DB4C0", // default
-          //            borderLeftWidth: iCol % 3 === 0 ? "3px" : "1px",
-          //            borderRightWidth: (iCol + 1) % 3 === 0 ? "3px" : "1px",
-          //            borderTopWidth: iRow % 3 === 0 ? "3px" : "1px",
-          //            borderBottomWidth: (iRow + 1) % 3 === 0 ? "3px" : "1px",
-
-          //            display: e === emptyCell ? "grid" : "flex",
-          //          }}
           >
             {e !== emptyCell
               ? charMap.get(settings.chars)?.get(e)
